fd4b2a233a61b923e50c34d04584267114e7ec8f
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-09 20:01:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash{:dennis_ritchie => {[:languages] = "assembly "}}
+    programmer_hash[:dennis_ritchie][:languages] = "assembly "
   return programmer_hash
 end
